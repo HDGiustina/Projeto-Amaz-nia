51ff38d3241166cfa3d967a1e5f01a86ae41b039
Helena Della Giustina <haguiar@imply.com>
2024-11-15 17:49:15 -0300

    ci: Ajustando configurações iniciais

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import './style.css'
+
+import App from './App.vue'
+import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(createVuetify({
+    components,
+    directives,
+}))
+
+
+app.mount('#app')
